c7ccc6ce3193832affe812489a12b59cce74b2b9
Rataphon Chitnorm <t.bombadua@gmail.com>
2017-09-18 08:06:29 +0700

    dynamic button sample

--- sample/src/main/java/com/stfalcon/chatkit/sample/features/demo/def/DefaultMessagesActivity.java
+++ sample/src/main/java/com/stfalcon/chatkit/sample/features/demo/def/DefaultMessagesActivity.java
@@ -3,7 +3,14 @@ package com.stfalcon.chatkit.sample.features.demo.def;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
@@ -23,6 +30,7 @@ public class DefaultMessagesActivity extends DemoMessagesActivity
     }
 
     private MessagesList messagesList;
+    private MessageInput messageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +41,45 @@ public class DefaultMessagesActivity extends DemoMessagesActivity
         initAdapter();
 
         MessageInput input = (MessageInput) findViewById(R.id.input);
+        messageInput = input;
         input.setInputListener(this);
     }
 
+
     @Override
     public boolean onSubmit(CharSequence input) {
-        super.messagesAdapter.addToStart(
-                MessagesFixtures.getTextMessage(input.toString()), true);
+
+        messageInput.getButton().setVisibility(View.GONE);
+        messageInput.getAttachmentButton().setVisibility(View.GONE);
+        messageInput.getInputEditText().setVisibility(View.GONE);
+
+        HorizontalScrollView sv= new HorizontalScrollView(this);//(ScrollView) findViewById(R.id.svScroll);
+        sv.setLayoutParams(new ScrollView.LayoutParams(ScrollView.LayoutParams.WRAP_CONTENT,ScrollView.LayoutParams.WRAP_CONTENT));
+        messageInput.addView(sv,0);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+        sv.addView(layout);
+
+        for(int i = 0; i < 20; i++){
+            Button btnTag = new Button(this);
+            btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            btnTag.setText("btttt " + i);
+            btnTag.setId(i);
+            btnTag.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Perform action on click
+                    messageInput.getButton().setVisibility(View.VISIBLE);
+                    messageInput.getAttachmentButton().setVisibility(View.VISIBLE);
+                    messageInput.getInputEditText().setVisibility(View.VISIBLE);
+                    messageInput.removeViewAt(0);
+                }
+            });
+            layout.addView(btnTag,i, new ViewGroup.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        }
+
+//        super.messagesAdapter.addToStart(
+//                MessagesFixtures.getTextMessage(input.toString()), true);
         return true;
     }
 
